59336a1a52b7620540bcfd3f3c79268a8f1f1f00
Yongfang0123 <yqzn9@mail.missouri.edu>
2018-02-05 10:02:27 -0600

    init

---
+++ EngineerDashboard/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace EngineerDashboard.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ EngineerDashboard/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EngineerDashboard.Startup))]
+namespace EngineerDashboard
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
